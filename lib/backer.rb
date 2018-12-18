63cad6d6bb6b68fc4b4a927029295250002ba7ec
Marcus Plenty <marcusplenty@gmail.com>
2018-12-18 01:08:55 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,11 @@
+class Backer
+  attr_reader :name, :backed_projects
+  def initialize(name)
+    @backed_projects = []
+    @name = name
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+  end 
+end 
